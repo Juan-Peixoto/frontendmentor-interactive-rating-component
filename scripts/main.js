708ef48ae1af1bd813ecdb09712bb88ed244fba9
Juan Peixoto <juanpeixoto1998@gmail.com>
2022-08-20 07:51:04 -0300

    Finished the rating functionalities

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,19 @@
 const button = document.getElementById('button');
 const numbers = document.querySelectorAll(".number");
+const selectedNumberView = document.getElementById('selectedNumberView');
+let selectedNumber = 0;
+
+for (let i = 0; i < numbers.length; i++) {
+    numbers[i].addEventListener('click', () => {
+        for (let i = 0; i < numbers.length; i++) {
+            numbers[i].classList.remove('selected');
+        }
+
+        numbers[i].classList.add('selected');
+        selectedNumber = numbers[i].textContent;
+        selectedNumberView.innerHTML = selectedNumber;
+    });
+}
 
 button.addEventListener('click', () => {
     const ratingBox = document.getElementById('rating');
@@ -10,5 +24,3 @@ button.addEventListener('click', () => {
     thankYouBox.classList.remove('hide');
     thankYouBox.classList.add('show');
 });
-
-console.log(numbers);
